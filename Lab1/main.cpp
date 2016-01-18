4313ae88b3581854ec88fb893f66c13d4e3fb5ad
chickentendon <maxronning@gmail.com>
2016-01-18 11:14:20 -0600

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Max Ronning on 1/18/16.
+//  Copyright © 2016 Max Ronning. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
